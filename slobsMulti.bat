aacc2e21a6bd359b2b7571bf8ca62a94982e9316
letivalo <74377668+letivalo@users.noreply.github.com>
2021-03-03 20:20:03 -0700

    Create slobsMulti.bat
    The main batch file.

---
+++ slobsMulti.bat
@@ -0,0 +1,43 @@
+@echo off
+title slobsMulti v20.3 by Kaoanon
+:: Find my Twitch at KaolinnLive 
+
+:: Minimizes cmd window for script; ends killsnaz task if already active
+powershell -window minimized -command ""
+schtasks /delete /tn killsnaz /f
+
+::Kills Spotify process before starting Snip to prevent snip.txt readback error
+taskkill /f /im spotify.exe
+
+:: Locates and starts Snip.exe; Minimizes all windows to hide snip browser pop-up
+cd /d "%userprofile%\Desktop\slobsMulti\references\Snip"
+start Snip.exe
+timeout /t 1 /nobreak
+powershell -command "(new-object -com shell.application).minimizeall()"
+
+:: Locates and starts a new instance of Spotify.exe
+cd %appdata%\Spotify
+start spotify.exe
+powershell -command "(new-object -com shell.application).minimizeall()"
+
+:: Locates and starts Snaz.exe; Minimizes Snaz application on start
+:: NOTICE - Set ChronosDwn to autostart in Snaz settings; Configure ChronosDwn to preference
+cd /d "C:\Snaz"
+powershell -window minimized -command "start Snaz.exe"
+
+:: Locates and starts lumacros.exe, telling it to read the luastreamkeys.lua
+cd /d %userprofile%\Desktop\slobsMulti\references\luamacros
+start luamacros.exe luastreamkeys.lua
+
+:: Locates and starts SLOBS; sets SLOBS to current active window using switch.vbs in batch mode
+cd /d "C:\Program Files\Streamlabs OBS"
+start "" "Streamlabs OBS.exe"
+powershell  %userprofile%\Desktop\slobsMulti\references\switch.vbs //b /ilc "Streamlabs OBS.exe"
+
+:: Fetches current systime, adds on top specified minutes at .AddMinutes(here), saves as variable %endtime%
+for /f "delims=" %%G IN ('powershell "(get-date %time%).AddMinutes(15).ToString('HH:mm:ss')"') do set endtime=%%G
+
+:: Creates a onetime system task to kill Snaz.exe at time given in %endtime% to  corrispond with when chronosdwn is done
+schtasks /create /sc once /ST %endtime% /TN killsnaz /TR "taskkill /f /im Snaz.exe"
+
+exit
