736e52f54aac2dbbb9c2e3ffb5af4ef8d4d795d3
letivalo <74377668+letivalo@users.noreply.github.com>
2021-03-03 22:11:39 -0700

    Refactoring ... again

--- streamblastoff.bat
+++ streamblastoff.bat
@@ -1,8 +1,8 @@
 @echo off
-title slobsMulti v20.3 by Kaoanon
+title streamblastoff pre21.3 by letivalo
 :: Find my Twitch at KaolinnLive 
 
-:: Minimizes cmd window for script; ends killsnaz task if already active
+:: Minimizes cmd window for batch file; ends killsnaz task if already active
 powershell -window minimized -command ""
 schtasks /delete /tn killsnaz /f
 
@@ -10,12 +10,12 @@ schtasks /delete /tn killsnaz /f
 taskkill /f /im spotify.exe
 
 :: Locates and starts Snip.exe; Minimizes all windows to hide snip browser pop-up
-cd /d "%userprofile%\Desktop\slobsMulti\references\Snip"
+cd /d "%userprofile%\Desktop\streamblastoff\libraries\binaries\Snip"
 start Snip.exe
 timeout /t 1 /nobreak
 powershell -command "(new-object -com shell.application).minimizeall()"
 
-:: Locates and starts a new instance of Spotify.exe
+:: Locates and starts a new instance of Spotify.exe; Minimizes all windows since Spotify cannot be minimized using powershell "-window minimized"
 cd %appdata%\Spotify
 start spotify.exe
 powershell -command "(new-object -com shell.application).minimizeall()"
@@ -26,13 +26,13 @@ cd /d "C:\Snaz"
 powershell -window minimized -command "start Snaz.exe"
 
 :: Locates and starts lumacros.exe, telling it to read the luastreamkeys.lua
-cd /d %userprofile%\Desktop\slobsMulti\references\luamacros
+cd /d %userprofile%\Desktop\streamblastoff\libraries\binaries\luamacros
 start luamacros.exe luastreamkeys.lua
 
 :: Locates and starts SLOBS; sets SLOBS to current active window using switch.vbs in batch mode
 cd /d "C:\Program Files\Streamlabs OBS"
 start "" "Streamlabs OBS.exe"
-powershell  %userprofile%\Desktop\slobsMulti\references\switch.vbs //b /ilc "Streamlabs OBS.exe"
+powershell  %userprofile%\Desktop\streamblastoff\libraries\switch.vbs //b /ilc "Streamlabs OBS.exe"
 
 :: Fetches current systime, adds on top specified minutes at .AddMinutes(here), saves as variable %endtime%
 for /f "delims=" %%G IN ('powershell "(get-date %time%).AddMinutes(15).ToString('HH:mm:ss')"') do set endtime=%%G
